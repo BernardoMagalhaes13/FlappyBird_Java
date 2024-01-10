463ac29fdaa0579979557e31703e4f305fc3ee2d
Dario <A038042@umaia>
2024-01-10 22:08:44 +0000

    SeedTest Final

---
+++ src/test/java/FlappyBird/Tests/SeedTest.java
@@ -0,0 +1,39 @@
+package FlappyBird.Tests;
+
+import com.flappybirdg07.Game.Seed;
+import com.flappybirdg07.Game.Position;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SeedTest {
+
+    @Test
+    public void Constructorseed() {
+        Position position = new Position(0, 0);
+
+        Seed seed = new Seed(position);
+
+        assertEquals(position, seed.getPosition());
+
+        Position position2 = new Position(10, 10);
+        seed.setPosition(position2);
+
+        assertEquals(position2, seed.getPosition());
+
+    }
+
+    @Test
+    public void seedActive() {
+        Position position = new Position(0, 0);
+
+        Seed seed = new Seed(position);
+
+        assertEquals(true, seed.isActive());
+        seed.setActive(false);
+
+        assertEquals(false, seed.isActive());
+
+    }
+}
