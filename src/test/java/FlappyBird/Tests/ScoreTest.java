00a112a2f68711c139ddd3876c4f9b4d5f6fbbe3
DarioRodrigues17 <133675148+DarioRodrigues17@users.noreply.github.com>
2024-01-10 23:10:30 +0000

    ScoreTest final

--- src/test/java/FlappyBird/Tests/ScoreTest.java
+++ src/test/java/FlappyBird/Tests/ScoreTest.java
@@ -1,2 +1,36 @@
-package FlappyBird.Tests;public class ScoreTest {
+package FlappyBird.Tests;
+
+
+import com.flappybirdg07.Game.Position;
+import com.flappybirdg07.Game.Score;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class ScoreTest {
+    @Test
+    public void Logicscore() {
+        Position position = new Position(10, 10);
+        Score score = new Score(position);
+
+        assertEquals(5, Score.getScorePerPipe());
+        assertEquals(0, score.getScoreTracker());
+
+        score.increaseScore();
+
+        assertEquals(5, score.getScoreTracker());
+        score.increaseScore();
+        assertNotEquals(5, score.getScoreTracker());
+    }
+
+    @Test
+    public void Constructorscore() {
+        Position position = new Position(10, 10);
+        Score score = new Score(position);
+
+        assertEquals(position, score.getPosition());
+    }
+
+
 }
